fe945b0e0b6dc433d0156c8c9bcc8a3ea86d98ec
274148916@qq.com <274148916@qq.com>
2025-12-01 15:37:09 +0800

    update

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -8843,7 +8843,7 @@ int main(int argc, char** argv) {
   gettimeofday(&tv, &tz);
   srandom(tv.tv_sec ^ tv.tv_usec ^ getpid());
 
-  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:QN:D:W:w:e:P:KEq:s:RFc:l:b:h:")) > 0)
+  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:QN:D:W:w:e:P:KEq:s:RFc:l:b:h:r:")) > 0)
 
     switch (opt) {
 
